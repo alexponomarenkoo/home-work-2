526900aa64ebd1fff6049985e71ebb46ae5f34a8
alexponomarenkoo <el4uva4ello@gmail.com>
2023-12-18 22:32:03 +0200

    completed task 1

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# # Користувач вводить три цифри з клавіатури. Залежно від вибору користувача програма
+#  # виводить на екран максимум із трьох,
+#  # мінімум із трьох або середньоарифметичне трьох чисел.
+# n1 = int(input("Enter first number: "))
+# n2 = int(input("Enter second number: "))
+# n3 = int(input("Enter third number: "))
+#
+# if n1 > n2 > n3:
+#     print(n1)
+# elif n2 > n3 > n1:
+#     print(n2)
+# elif n3 > n2 > n1:
+#     print(n3)
+#
+# if n1 < n2 < n3:
+#     print(n1)
+# elif n2 < n3 < n1:
+#     print(n2)
+# elif n3 < n2 < n1:
+#     print(n3)
+#
+# Average = (n1+n2+n3)/3
+# print(Average)
